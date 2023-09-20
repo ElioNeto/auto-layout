cef50703bc207616bb3c58577079bfa4fb476f1a
Elio <netoo.elio@hotmail.com>
2023-09-20 17:34:41 -0300

    remove unused imports

--- src/app/shared/components/page/page.component.ts
+++ src/app/shared/components/page/page.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-page",
   templateUrl: "./page.component.html",
   styleUrls: ["./page.component.css"],
 })
-export class PageComponent implements OnInit {
+export class PageComponent {
   @Input() page: any;
   @Input() data: any;
   @Input() sections: any;
   @Input() contents: any;
-
-  constructor() {}
-  ngOnInit(): void {}
 }
--- src/app/shared/components/section/section.component.ts
+++ src/app/shared/components/section/section.component.ts
@@ -1,16 +1,13 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-section",
   templateUrl: "./section.component.html",
   styleUrls: ["./section.component.css"],
 })
-export class SectionComponent implements OnInit {
+export class SectionComponent {
   @Input() sections: any;
   @Input() contents: any;
   @Input() data: any;
   @Input() sectionsInPage: any;
-
-  constructor() {}
-  ngOnInit(): void {}
 }
