db27692c324d24979c5f2d898599caf1be6baf53
Elio <netoo.elio@hotmail.com>
2023-09-20 10:49:51 -0300

    resolve conflits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CoreComponent } from './core/core.component';
-import { PageComponent } from './shared/components/page/page.component';
-import { SectionComponent } from './shared/components/section/section.component';
-import { OrchestratorComponent } from './shared/components/orchestrator/orchestrator.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoreComponent } from "./core/core.component";
+import { PageComponent } from "./shared/components/page/page.component";
+import { SectionComponent } from "./shared/components/section/section.component";
+import { OrchestratorComponent } from "./shared/components/orchestrator/orchestrator.component";
 
 @NgModule({
   declarations: [
@@ -14,13 +14,10 @@ import { OrchestratorComponent } from './shared/components/orchestrator/orchestr
     CoreComponent,
     PageComponent,
     SectionComponent,
-    OrchestratorComponent
+    OrchestratorComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
